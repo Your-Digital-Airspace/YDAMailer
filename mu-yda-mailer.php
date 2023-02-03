fd928d38d716cdf9eff5024fe143e98d19a64699
Andy James <andy@andyjames.org>
2023-02-02 20:10:13 -0600

    commiting code

---
+++ mu-yda-mailer.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * GridPane Mailer
+ *
+ * @wordpress-plugin
+ * Plugin Name:      YDA Mailer
+ * Description:      Plugin to use SMTP mail services instead of wp_mail
+ * Version:          1.0.0
+ * Author:           Chad Butler, Jeff Cleverley, Andy James
+ * License:          GPL-2.0+
+ * License URI:      http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain:      yda-mailer
+ */
+
+require __DIR__ . '/yda-mailer/yda-mailer.php';
